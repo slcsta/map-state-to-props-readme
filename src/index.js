24a173117260b746dc14327c8aaea615966ff58c
Sarah <sarahlchavez@gmail.com>
2021-11-27 13:44:07 -0800

    commit tree b279c5838a9f07838212ee1eedcf529d2f4b116f

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from "react-redux";
 import counterReducer from './reducers/counterReducer.js';
 import App from './App';
 import './index.css';
@@ -10,4 +11,12 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ); 
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+// imported Provider from React Redux
+// used Provider to wrap our React application
+// passed our store instance directly into the App component, we now pass it in to Provider as a prop, which will make it available to other components
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>, 
+    document.getElementById('root')
+);
